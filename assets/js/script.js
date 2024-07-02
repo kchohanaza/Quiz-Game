bfcbabb9c0755967d5dc1de9bf2fe50cbf3d6c5c
Neeru Rathour <Neerurathour@gmail.com>
2024-07-02 10:13:54 +0000

    add question answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,52 @@
-console.log("Golden Quizz app welcomes you");
+//option questions ans answers
+const questions =[
+    
+    {
+     question: "What is the term for a score of three under par on a single hole",
+     answers:[
+         {text:"Eagle", correct: false},
+         {text: "Double Eagle", correct: false},
+         {text: "Albatross", correct: true},
+         {text: "Birdie", correct: false},
+     ]
+    },
+    {
+     question: "What do you call the grassy area surrounding the putting green?",
+     answers:[
+         {text:"Rough", correct: false},
+         {text: "Fairway", correct: false},
+         {text: "Fringe", correct: true},
+         {text: "Hazard", correct: false},
+     ]
+    },
+    {
+     question: "In which country did the game of golf originate?",
+     answers:[
+         {text:"USA", correct: false},
+         {text: "Scotland", correct: true},
+         {text: "Ireland", correct: false},
+         {text: "England", correct: false},
+     ]
+    },
+
+    { 
+        question: " What is the standard number of holes on a golf course?",
+        answers:[
+            {text:"9", correct: false},
+            {text: "18", correct: true},
+            {text: "27", correct: false},
+            {text: "36", correct: false},
+        ]
+       },
+       { 
+        question: " Which major golf tournament is held at different courses each year?",
+        answers:[
+            {text:"The Masters", correct: false},
+            {text: "The Open Championship", correct: false},
+            {text: "U.S. Open", correct: true},
+            {text: "PGA Championship", correct: false},
+        ]
+       },
+
+ ];
+
